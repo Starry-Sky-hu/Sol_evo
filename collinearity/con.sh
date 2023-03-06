7e3409d38c2fb31fd9db89c0c4daa429ee931d6a
Starry-Sky-hu <53527045+Starry-Sky-hu@users.noreply.github.com>
2023-03-06 17:36:44 +0800

    Update con.sh

--- collinearity/con.sh
+++ collinearity/con.sh
@@ -7,11 +7,3 @@ do
     cd ..
 done
 
-for i in $(cat gff_modify_list)
-do
-    mkdir -p ${i}
-    cd ${i}
-    cp ../genetribe_gff_modify.sh ${i}_genetribe.sh
-    sed -i 's/protein/'${i}'/g' ${i}_genetribe.sh
-    cd ..
-done
