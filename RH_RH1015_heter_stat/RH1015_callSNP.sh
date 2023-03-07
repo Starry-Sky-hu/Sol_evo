77455e5ff1d80277a3a885eeb2aeebcc702e1c82
Starry-Sky-hu <53527045+Starry-Sky-hu@users.noreply.github.com>
2023-03-07 20:34:37 +0800

    Update RH1015_callSNP.sh

--- RH_RH1015_heter_stat/RH1015_callSNP.sh
+++ RH_RH1015_heter_stat/RH1015_callSNP.sh
@@ -1,3 +1,8 @@
+
+minimap2 -t 52 -ax map-hifi Solanum_tuberosumDM.fa RH1015.ccs.fa.gz > DM_v6_RH1015_aln.sam
+samtools sort DM_v6_RH1015_aln.sam -O bam -@ 52 -m 2G > DM_v6_RH1015_aln.sort.bam
+samtools index DM_v6_RH1015_aln.sort.bam
+
 for i in {01..12}
 do
         echo "bcftools mpileup -X pacbio-ccs -Ou -r chr${i} -f Solanum_tuberosumDM.fa DM_v6_RH1015_aln.sort.bam | bcftools call -m -Ov -V indels -o DM_v6_RH1015_chr${i}.gvcf" >> call.sh
