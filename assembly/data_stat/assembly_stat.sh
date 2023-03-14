c9f448fb8e77c1b566f39561587234b12fb1dc1d
Starry-Sky-hu <53527045+Starry-Sky-hu@users.noreply.github.com>
2023-03-14 09:43:33 +0800

    Update assembly_stat.sh

--- assembly/data_stat/assembly_stat.sh
+++ assembly/data_stat/assembly_stat.sh
@@ -13,36 +13,36 @@
 # @Description:
 ##################################################################
 
-#for i in $(cat hic_list)
-#do
-#       pigz -c -p 52 -k -d /home/wuyaoyao/03-Solanaceae/01_Assembly/02_HiC/01_Primary_Contig/Clean_contig/${i}_hifiasm.hic.p_ctg_clean.fasta.gz > ${i}.fa
-#done
-#
-#for i in $(cat nohic_list)
-#do
-#       cp /home/wuyaoyao/03-Solanaceae/05_PartOne/01_Ref/${i}.fa .
-#done
-#
-#
-#ls *fa > list
-#for i in $(cat list)
-#do
-#       seqkit stat -a -G "N" ${i} >> stat_results
-#done
-#
-#for i in $(cat list)
-#do
-#       seq_n50.pl ${i} > ${i%.fa*}
-#done
-#for i in $(cat list); do echo -n "${i} "; grep "N90" ${i%.fa*} | cut -f2; done > N90_stat
+for i in $(cat hic_list)
+do
+       pigz -c -p 52 -k -d /home/wuyaoyao/03-Solanaceae/01_Assembly/02_HiC/01_Primary_Contig/Clean_contig/${i}_hifiasm.hic.p_ctg_clean.fasta.gz > ${i}.fa
+done
 
-#pigz -c -p 52 -k -d /home/wuyaoyao/03-Solanaceae/01_Assembly/02_HiC/01_Primary_Contig/Clean_contig/Solanum_ochranthum_hifiasm.l3.hic.hap1.p_ctg_clean.fasta.gz > Solanum_ochranthum.fa
-#seqkit stat -a -G "N" Solanum_ochranthum.fa
-#seq_n50.pl Solanum_ochranthum.fa > Solanum_ochranthum
+for i in $(cat nohic_list)
+do
+       cp /home/wuyaoyao/03-Solanaceae/05_PartOne/01_Ref/${i}.fa .
+done
 
-#sed 's/list/xaa/g' busco.sh > busco_a.sh
-#sed 's/list/xab/g' busco.sh > busco_b.sh
-#sed 's/list/xac/g' busco.sh > busco_c.sh
-#sed 's/list/xad/g' busco.sh > busco_d.sh
 
-#grep "C:" *.fa_busco/short* | awk -F '10.' '{print $2}'
+ls *fa > list
+for i in $(cat list)
+do
+       seqkit stat -a -G "N" ${i} >> stat_results
+done
+
+for i in $(cat list)
+do
+       seq_n50.pl ${i} > ${i%.fa*}
+done
+for i in $(cat list); do echo -n "${i} "; grep "N90" ${i%.fa*} | cut -f2; done > N90_stat
+
+pigz -c -p 52 -k -d /home/wuyaoyao/03-Solanaceae/01_Assembly/02_HiC/01_Primary_Contig/Clean_contig/Solanum_ochranthum_hifiasm.l3.hic.hap1.p_ctg_clean.fasta.gz > Solanum_ochranthum.fa
+seqkit stat -a -G "N" Solanum_ochranthum.fa
+seq_n50.pl Solanum_ochranthum.fa > Solanum_ochranthum
+
+sed 's/list/xaa/g' busco.sh > busco_a.sh
+sed 's/list/xab/g' busco.sh > busco_b.sh
+sed 's/list/xac/g' busco.sh > busco_c.sh
+sed 's/list/xad/g' busco.sh > busco_d.sh
+
+grep "C:" *.fa_busco/short* | awk -F '10.' '{print $2}'
